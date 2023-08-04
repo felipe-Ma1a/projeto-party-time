08a04b3604fba1a2e6a85c2789f5732c85393c19
Felipe Maia <felipemaia850@gmail.com>
2023-08-04 11:36:20 -0300

    link update

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const partyFetch = axios.create({
-  baseURL: "https://party-time-back.vercel.app/api/",
+  baseURL: "https://party-time-back.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
